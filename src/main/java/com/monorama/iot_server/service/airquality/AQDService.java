b3c671de14d228c26077bb06b9878efb9c47d2ba
Sangmin Han <gkstkdals07@naver.com>
2026-02-26 09:44:23 +0900

    :art: Refactor: 로그작성

--- src/main/java/com/monorama/iot_server/service/airquality/AQDService.java
+++ src/main/java/com/monorama/iot_server/service/airquality/AQDService.java
@@ -1,5 +1,6 @@
 package com.monorama.iot_server.service.airquality;
 
+import com.monorama.iot_server.domain.AirQualityData;
 import com.monorama.iot_server.domain.User;
 import com.monorama.iot_server.dto.request.airquality.AQDRequestDto;
 import com.monorama.iot_server.dto.request.airquality.AQDSyncRequestDto;
@@ -8,12 +9,14 @@ import com.monorama.iot_server.exception.ErrorCode;
 import com.monorama.iot_server.repository.AQDRepository;
 import com.monorama.iot_server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AQDService {
 
@@ -25,7 +28,11 @@ public class AQDService {
         User user = userRepo.findById(userId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
 
+        log.info("before make Entity : {}", dto.createdAt());
         List<String> missingFields = dto.getMissingFields(user);
+
+        AirQualityData result = dto.toEntity(user);
+        log.info("after make Entity : {}", result.getCreatedAt());
         airRepo.save(dto.toEntity(user));
 
         return missingFields.isEmpty() ?
